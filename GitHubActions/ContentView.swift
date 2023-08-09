b29ae0e30137d0ec794b7006436a1ed7df352038
Thaynara-Andrade <thaynara.silva.andrade07@aluno.ifce.edu.br>
2023-08-09 09:07:08 -0300

    Initial Commit

---
+++ GitHubActions/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubActions
+//
+//  Created by Thaynara da Silva Andrade on 09/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubActions/GitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsApp.swift
+//  GitHubActions
+//
+//  Created by Thaynara da Silva Andrade on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
